c24d0fb4d6c0423df5c183742490373c005c8ec7
Aavin Fernando <aavin.fernando@gmail.com>
2023-08-21 21:01:53 +1000

    he he he ha

--- Scripts/WorldManager.cs
+++ Scripts/WorldManager.cs
@@ -1,12 +1,26 @@
 using Godot;
 using System;
+using System.Runtime.InteropServices;
 
-public partial class WorldManager : Node {
-    // Called when the node enters the scene tree for the first time.
+public partial class WorldManager : GenericSingleton<Node> {
+    public override void _EnterTree() {
+        base._EnterTree();
+    }
+
     public override void _Ready() {
+
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
+        
     }
+
+    public struct GameStatus { // Should contain enough data that we can save and load the game
+        // an instance of a ResourceInfo struct from Jaden
+        public int worldSeed;
+        public int a;
+    }
+
+
+    public GameStatus m_status;
 }
