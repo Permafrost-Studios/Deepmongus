4a382831d3adf370d66c57a5783701664e7f903f
collab112 <jadenchoi6@gmail.com>
2023-08-21 23:10:36 +1000

    bruhasdasdsadas

--- Scripts/Inventory/ResourceManager.cs
+++ Scripts/Inventory/ResourceManager.cs
@@ -6,7 +6,7 @@ public class ResourceManager : Node {
 	int torpedoes;
 	int batteries;
 
-	private void _process(Node2D body) {
+	private void _process(Node2D node) {
 
 		switch( (node.GetParent() as Resource).type ) {
 
